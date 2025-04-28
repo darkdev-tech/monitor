ca99e4d5311be1ffb632f878f4771f986a29e2d0
Dark hacker 😈 <javieldev@gmail.com>
2025-04-28 08:51:23 +0300

    Update ping.js

--- plugins/ping.js
+++ plugins/ping.js
@@ -32,7 +32,7 @@ const ping = async (m, Matrix) => {
         forwardingScore: 999,
         isForwarded: true,
         forwardedNewsletterMessageInfo: {
-          newsletterJid: '120363391979107532@newsletter',
+          newsletterJid: '120363354023106228@newsletter',
           newsletterName: "XΞNØCRYPT",
           serverMessageId: 143
         }
