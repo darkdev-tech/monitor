812546b4051990279bc58f062628266dce0bab7a
Dark hacker 😈 <javieldev@gmail.com>
2025-04-28 08:49:03 +0300

    Update main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -234,7 +234,7 @@ const menu = async (m, Matrix) => {
         forwardingScore: 999,
         isForwarded: true,
         forwardedNewsletterMessageInfo: {
-          newsletterJid: '120363391979107532@newsletter',
+          newsletterJid: '120363354023106228@newsletter',
           newsletterName: "XΞNØCRYPT",
           serverMessageId: 143
         }
